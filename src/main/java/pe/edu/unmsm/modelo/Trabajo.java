e0c8d0bd46a4cbdc658efc85f5c09fcc2e39cc17
caritosteph <10200194@unmsm.edu.pe>
2014-12-01 14:15:48 -0500

    editar

---
+++ src/main/java/pe/edu/unmsm/modelo/Trabajo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.unmsm.modelo;
+import org.javalite.activejdbc.Model;
+
+/**
+ *
+ * @author culfe
+ */
+public class Trabajo extends Model{
+    
+}
--- src/migrations/20141101170851_crear_tabla_usuario.sql
+++ src/migrations/20141101170851_crear_tabla_usuario.sql
@@ -26,12 +26,24 @@ CREATE TABLE IF NOT EXISTS "SSE"."alumnos" (
         "facebook" varchar(20),
         "linkedin" varchar(20),
         "twitter" varchar(20),
-        
 	"cv_url" varchar(50),
 	"foto_url" varchar(50),
-
 	"usuario_id" integer,
+        "trabajo_id" integer,
         "estado_civil" varchar(50),
         "genero" varchar(50), 
 	PRIMARY KEY(ID)
 );
+
+CREATE TABLE IF NOT EXISTS "SSE"."trabajos" (
+	"id" integer generated always as identity NOT NULL,
+	"nombre_empresa" varchar(50),
+        "fecha_inicio" date,
+        "fecha_fin" date,
+        "actual" boolean,
+        "direccion_empresa" varchar(8),
+        "titulo_puesto" varchar(20), 
+        "descripcion_puesto" varchar(100),
+	PRIMARY KEY(ID)
+);
+
